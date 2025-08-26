f81cfea71b4b961e1c7daf82d8f307ad9b3bc194
Safe Pump <powelljm@my.lanecc.edu>
2025-08-26 02:45:16 +0000

    Add discriminator crate and update token to use local spl-discriminator for safe-pump compatibility

--- libraries/discriminator/src/lib.rs
+++ libraries/discriminator/src/lib.rs
@@ -1,19 +1,1 @@
-use solana_program::program_error::ProgramError;
 
-pub trait Discriminator {
-    const DISCRIMINATOR: [u8; 8];
-
-    fn discriminator() -> [u8; 8] {
-        Self::DISCRIMINATOR
-    }
-}
-
-#[macro_export]
-macro_rules! discriminator {
-    ($discriminator:expr) => {
-        #[automatically_derived]
-        impl Discriminator for $discriminator {
-            const DISCRIMINATOR: [u8; 8] = [0; 8]; // Placeholder, replace with actual discriminator
-        }
-    };
-}
